43f474a55e32a34f53bbc2374786ddee531773e5
hsafir <safirahamed317@gmail.com>
2018-06-08 17:25:29 +0530

    Create app.go

---
+++ app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+)
+
+func main(){
+
+	router := mux.NewRouter()
+	router.HandleFunc("/",info).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8084",router))
+}
+
+func info(w http.ResponseWriter, req *http.Request){
+	w.Write([]byte("dummy web page!!"))
+}
